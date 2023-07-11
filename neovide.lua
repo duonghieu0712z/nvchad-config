1593476a7d3eeef23ef853f7bc5de80c451a8de6
Dương Hiếu <duonghieu0712z@gmail.com>
2023-07-11 17:52:42 +0700

    Update neovide file

--- neovide.lua
+++ neovide.lua
@@ -1,15 +1,18 @@
 local g = vim.g
+local o = vim.o
 
 local transparency = 0.92
 
 local alpha = function()
-  return string.format("%x", math.floor(255 * vim.g.transparency or transparency))
+  return string.format("%x", math.floor(255 * g.transparency or transparency))
 end
 
 if g.neovide then
-  g.guifont = "CaskaydiaCove NF:h12"
+  o.guifont = "FiraCode Nerd Font:h13"
 
   g.neovide_transparency = transparency
   g.transparency = transparency
   g.neovide_background_color = "#0f1117" .. alpha()
+
+  g.neovide_hide_mouse_when_typing = true
 end
