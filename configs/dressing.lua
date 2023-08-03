e99bd50321fec66b6a592483ef5604a095c190f5
Dương Hiếu <duonghieu0712z@gmail.com>
2023-08-04 00:03:19 +0700

    Update project

--- configs/dressing.lua
+++ configs/dressing.lua
@@ -1,7 +1,6 @@
 local M = {
   select = {
     get_config = function(opts)
-      print(opts.kind)
       if opts.kind == "codeaction" then
         return {
           telescope = require("telescope.themes").get_cursor {},
